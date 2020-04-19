5cf7ebc8c50f4bb8a40cadd8a85168c24202b5f7
Chris Larsen <clarsendev@protonmail.com>
2020-04-19 02:01:49 -0400

    Add comments

--- src/components/Comparator.vue
+++ src/components/Comparator.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
-    <div class="description" v-html="description"></div>
+    <div class="description">
+      <p>Ever want to know where your income sits compared to other Canadians? Want to shut up your rich friends when they claim they're not rich?</p>
+      <p>Simply enter your (or anyone's) yearly income for tax year {{year}} into the box below, and you'll see where that income sits against that of other Canadians.</p>
+    </div>
     <input placeholder="Enter your income" v-model.number="income" type="number" class="income"/>
     <p class="answer">{{result}}</p>
+    <p class="comment" v-html="comment"></p>
     <footer>Data is sourced from the <a href="https://www.canada.ca/en/revenue-agency/programs/about-canada-revenue-agency-cra/income-statistics-gst-hst-statistics/t1-final-statistics/2017-tax-year.html" target="_blank">Canadian Revenue Agency</a></footer>
   </div>
 </template>
@@ -15,7 +19,6 @@ export default {
   name: 'Comparator',
   data() {
     return {
-      description: `<p>Ever want to know where your income sits compared to other Canadians? Want to shut up your rich friends when they claim they're not rich?</p>\n<p>Simply enter your (or anyone's) yearly income for tax year ${year} into the box below, and you'll see where that income sits against that of other Canadians.</p>`,
       income: 0,
 
       year,
@@ -59,6 +62,23 @@ export default {
       return this.values.map(value => {
         return value.number;
       });
+    },
+    comment() {
+      if (this.income <= 0) {
+        return "";
+      } else if (this.above > 0.8) {
+        return "You don't make a lot of money. Hopefully you're only working part time, and studying to find a better career. If you're making this income at full time, then you're very poor. In fact, your low income is an expression of society's failure. You have every right to demand more money.";
+      } else if (this.above > 0.60) {
+        return "You're poor. There's no way to sugarcoat it. Your income reflects the failure of our economy to ensure that everyone is making a liveable wage.";
+      } else if (this.above > 0.5) {
+        return "You're below the halfway mark. More than half of Canadians make more money than you. You're definitely not very well off, but things could be much worse, as they are for a great deal of Canadians."
+      } else if (this.below > 0.98) {
+        return "You're obscenely rich. With an income this high, there's a very good chance that you're part of the 1%. You know, one of those people that were the reason for all the occupy protests? Yeah, that's your fault.";
+      } else if (this.below > 0.8) {
+        return "You're upper class. You make more money than 80% of Canadians. There's no good reason for you to be wanting for more money. In fact, increasing wages for people as rich as you has been shown to make things more difficult for those in the lower income ranges. So instead of complaining that your car or house isn't expensive enough, try being grateful for having so much already.";
+      } else {
+        return "You're neither rich nor poor. You're a solid middle class worker. As long as you're reasonably smart about your money, and don't have unexpected financial problems, you can easily afford a reasonable standard of living.";
+      }
     }
   },
   methods: {
@@ -83,5 +103,10 @@ export default {
     bottom: 5%;
     width: 100%;
     text-align: center;
+  }
+
+  p {
+    width: 80%;
+    margin: 1em 10%;
   }
 </style>
